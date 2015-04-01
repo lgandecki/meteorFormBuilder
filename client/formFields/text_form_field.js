00c178ffda5baa63088e0544800fbb8a33badca0
lgandecki <lgandecki@css.edu>
2015-04-01 11:07:36 -0500

    code refactor

--- client/formFields/text_form_field.js
+++ client/formFields/text_form_field.js
@@ -6,9 +6,8 @@ Template.text_form_field.created = function() {
 Template.text_form_field.helpers({
     "value": function() {
         var _form;
-        if (_isArray()) {
-            var _parentData = Template.parentData(1);
-            _form = _parentData;
+        if (_isNestedObject()) {
+            _form = Template.parentData(1);
         } else {
             _form = Forms.findOne({_id: parentId});
         }
@@ -21,20 +20,12 @@ Template.text_form_field.events({
         var _newValue = $(event.target).val();
         var _setQuery = {$set: {}};
 
-        if (_isArray()) {
-            var _form = Forms.findOne({_id: parentId});
-
-            var _row = _getRowContext();
-
-            var _arrayContext = _getArrayContext();
-            var _rowIndex = _.indexOf(_.pluck(_form[_arrayContext.dbField], 'id'), _row.id);
-            _setQuery["$set"][_arrayContext.dbField + "." + _rowIndex + "." + this.dbField] = _newValue;
-            console.log("_setQuery", _setQuery);
+        if (_isNestedObject()) {
+            _setQuery = _returnNestedObjectUpdateQueryWith(_newValue, this.dbField);
 
         } else {
-            _setQuery["$set"][this.dbField] = _newValue;
+            _setQuery = _returnObjectUpdateQueryWith(_newValue, this.dbField)
         }
-
 
         Forms.update({_id: parentId}, _setQuery);
     }
@@ -43,7 +34,7 @@ Template.text_form_field.events({
 
 var _getParentId = function() {
     var _parentData;
-    if (_isArray()) {
+    if (_isNestedObject()) {
          _parentData = Template.parentData(3);
     } else {
          _parentData = Template.parentData(1);
@@ -51,9 +42,28 @@ var _getParentId = function() {
     return _parentData.id;
 };
 
-var _isArray = function() {
+var _isNestedObject = function() {
     var _parentData = Template.parentData(2);
     return _parentData && _parentData.table;
+};
+
+var _returnNestedObjectUpdateQueryWith = function(newValue, dbField) {
+    var _setQuery = {$set: {}};
+    var _form = Forms.findOne({_id: parentId});
+    var _row = _getRowContext();
+    var _arrayContext = _getArrayContext();
+
+    var _rowIndex = _.indexOf(_.pluck(_form[_arrayContext.dbField], 'id'), _row.id);
+
+    _setQuery["$set"][_arrayContext.dbField + "." + _rowIndex + "." + dbField] = newValue;
+
+    return _setQuery;
+};
+
+var _returnObjectUpdateQueryWith = function(newValue, dbField) {
+    var _setQuery = {$set: {}};
+    _setQuery["$set"][dbField] = newValue;
+    return _setQuery;
 };
 
 var _getRowContext = function(){
@@ -62,4 +72,4 @@ var _getRowContext = function(){
 
 var _getArrayContext = function() {
     return Template.parentData(2);
-}
+};
