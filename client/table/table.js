59780227ee9dd50ee0c48a6e5c58a9e079600786
Peter Brejcha <peter.brejcha@gmail.com>
2015-04-01 13:12:25 -0500

    removing row upon delete button click in table

--- client/table/table.js
+++ client/table/table.js
@@ -1,6 +1,8 @@
 var parentId = null;
+var dbField;
 Template.table.created = function() {
     parentId = _getParentId();
+    dbField = this.data.dbField;
 };
 
 Template.table.helpers({
@@ -10,6 +12,18 @@ Template.table.helpers({
     }
 });
 
+Template.table.events({
+    'click .deleteRow': function(e){
+        var _setQuery = {$set: {}};
+        var _form = Forms.findOne({'_id':parentId});
+        var _rowIndex = _.indexOf(_.pluck(_form[dbField], 'id'), this.id);
+
+        _form[dbField].splice(_rowIndex, 1);
+        _setQuery['$set'][dbField] = _form[dbField];
+
+        Forms.update({'_id': parentId}, _setQuery);
+    }
+});
 
 var _getParentId = function() {
     return Template.parentData(1).id;
